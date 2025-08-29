ed1997123085966153cab92060e8b4b51f2c7349
archie.mcewen <arcmce@gmail.com>
2025-08-29 16:09:56 +0100

    Fix top bar overlap.
    fix logo color on light mode

--- app/src/main/java/com/arcmce/boogaloo/ui/view/MainActivity.kt
+++ app/src/main/java/com/arcmce/boogaloo/ui/view/MainActivity.kt
@@ -6,14 +6,13 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Notifications
@@ -21,6 +20,7 @@ import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -29,17 +29,25 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -73,6 +81,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         val repository = Repository()
 
         val sharedViewModel: SharedViewModel by viewModels { SharedViewModelFactory(application) }
@@ -82,6 +92,17 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             BoogalooJetpackTheme {
+                val topBarColor = MaterialTheme.colorScheme.surface
+                val view = LocalView.current
+                val lightIcons = topBarColor.luminance() > 0.5f
+
+                SideEffect {
+                    // Control status/navigation bar icon contrast
+                    val controller = WindowInsetsControllerCompat(window, view)
+                    controller.isAppearanceLightStatusBars = lightIcons
+                    controller.isAppearanceLightNavigationBars = topBarColor.luminance() > 0.5f
+                }
+
                 AppContent(
                     liveViewModel = liveViewModel,
                     catchUpViewModel = catchUpViewModel,
@@ -106,7 +127,7 @@ class MainActivity : ComponentActivity() {
                 // Update data in the ViewModel or any other relevant logic
                 liveViewModel.fetchRadioInfo()
 
-                // Delay for 60 seconds
+                // Delay for 10 seconds
                 delay(10_000)
             }
         }
@@ -144,16 +165,20 @@ fun AppContent(
     ) {
         Scaffold(
             topBar = {
-                Icon(
-                    painter = painterResource(id = R.drawable.logo_long_white),
-                    contentDescription = "Boogaloo logo long black",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .wrapContentWidth(Alignment.Start)
-                        .padding(16.dp)
-                        .height(32.dp)
-                    , // Size can be adjusted as needed
-                    tint = MaterialTheme.colorScheme.onBackground
+                CenterAlignedTopAppBar(
+                    windowInsets = TopAppBarDefaults.windowInsets,
+                    title = {
+                        Image(
+                            painter = painterResource(if (isDarkTheme) R.drawable.logo_long_white else R.drawable.logo_long_black),
+                            contentDescription = "Boogaloo Radio",
+                            modifier = Modifier
+                                .height(32.dp),
+                            contentScale = ContentScale.Fit
+                        )
+                    },
+                    colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                        containerColor = Color.Transparent
+                    )
                 )
             },
             bottomBar = {
